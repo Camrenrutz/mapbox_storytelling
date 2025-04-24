224c387153ef4960f8501ebc8832d34a2db65c72
Camrenrutz <rutzcd@dukes.jmu.edu>
2025-04-23 23:40:13 -0400

    push

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ var config = {
     auto: false,
     title: 'Color Purple Essay 2',
     subtitle: 'Spatial Journeys across the South 2',
-    byline: 'My Name',
+    byline: 'Camren Rutz',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
