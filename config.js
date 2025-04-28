5f8c4e8f302b6aff8431e9a6b9f59934e54f3192
Camrenrutz <rutzcd@dukes.jmu.edu>
2025-04-28 12:32:04 -0400

    push

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Souther Literature Virtual Essay',
-    subtitle: 'Spatial Journeys across the South 2',
-    byline: 'Camren Rutz',
+    title: 'Southern Literature Virtual Essay',
+    subtitle: 'The Growth of Autonomy in the South',
+    byline: 'By Camren Rutz and Kyle Carey',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
