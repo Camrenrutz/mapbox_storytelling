7e1b03b4f934bb76a1a302c5a721f0a20d376c7d
Camrenrutz <rutzcd@dukes.jmu.edu>
2025-05-03 09:55:11 -0400

    push

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ var config = {
             description: 'add text',
             location: {
                 center: [-89.48848661057376, 34.28249827482276], 
-                zoom: 8.00,
+                zoom: 6.5,
                 pitch: 45.0,
                 bearing: 0.00
             },
